ce092df6e525ab298106cfa10253165239314503
Suresh5961 <Sureshkumarece87@gmail.com>
2022-08-12 10:15:05 +0530

    complete

---
+++ controlstatement.py
@@ -0,0 +1,31 @@
+# control statement
+
+a=10
+b=20
+c=30
+#if else
+'''if(a>b):
+    print(a)
+
+else:
+    print(b)
+'''
+#Nested if contitiom
+'''if(a<b):
+    if(b<c):
+        print(a)
+    else:
+         print(b)
+else:
+    print(c)'''
+
+#elif
+   
+if(a<b):
+    print(a)
+
+elif(b<c):
+    print(b)
+
+else:
+    print(c)
