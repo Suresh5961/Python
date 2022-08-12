b5cce7c40d276ce960465d7056eea05f9f718263
Suresh5961 <Sureshkumarece87@gmail.com>
2022-08-12 10:52:55 +0530

    hi

---
+++ comparison operator.py
@@ -0,0 +1,13 @@
+#comparition operator
+
+print(5>10)
+
+print(3>10)
+
+print(3==5)
+
+print(5>=5)
+
+print(5<=10)
+
+print(80<=79)
---
+++ first.py
@@ -0,0 +1,29 @@
+# # identifier: user defined name for object, class and functions
+# do's    : suresh, sure456sh, _suresh, _456
+# dont's  : sur esh,sure#$%sh, 345434, pass 
+
+
+# pendrive data
+model="Sandisk"
+capacity=16
+price=560
+version=3.1
+typ="otg"
+
+print(type(model),model)
+print(type(capacity),capacity)
+print(type(price),price)
+print(type(version),version)
+print(type(typ),typ)
+
+# mobile
+model="lenova"
+mobileprice="12000"
+vertion="2.0"
+color="black"
+size=15
+
+print(type(model),model)
+mobileprice=int(mobileprice)
+print(mobileprice,type(mobileprice))
+
