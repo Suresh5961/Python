9a0e6c03487da1ddfb5882707541ac83b5e7f2f7
Suresh5961 <Sureshkumarece87@gmail.com>
2022-08-17 11:21:11 +0530

    looping

---
+++ logical.py
@@ -0,0 +1,34 @@
+a=10
+b=20
+c=30
+# and 
+
+'''if(a>b & a>c):
+    print(a)
+
+else:
+    print(c)'''
+
+'''if(a<b and a<b):
+   print(a) 
+else:
+    print(c)
+
+
+if(a>b and a<b):
+    print(a)
+else:
+    print(c)
+'''
+
+#OR
+#Any one contition Ture 
+
+'''if (a>b or a<c):
+    print(a)
+else:
+    print(c)'''
+
+#not
+if(1!=0):
+    print("haii")
---
+++ looping.py
@@ -0,0 +1,18 @@
+for a in range(5):
+    print(a)
+
+for b in range(10):
+    print("aarthi")
+
+for c in "suresh":
+    print(c)
+
+for i in range(10,20,1):
+    print(i)
+
+'''for j in range(30,50,2):
+     print(j)
+'''
+
+'''for k in range (20,10,-2):
+    print(k)'''
