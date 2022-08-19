837f1f1de7c4deb92bc5217163dbb1c15ceab553
Suresh5961 <Sureshkumarece87@gmail.com>
2022-08-19 11:02:43 +0530

    pattern

--- looping.py
+++ looping.py
@@ -1,3 +1,5 @@
+#for loop
+
 for a in range(5):
     print(a)
 
--- nestedforloop.py
+++ nestedforloop.py
@@ -1,8 +1,31 @@
-for i in range(3):  #-->(0,1,2)
+#nested for loop
+
+'''for i in range(3):  #-->(0,1,2)
     for j in range(2):  #-->(0,1)
 
         print(j)
-    print(i)
+    print(i)'''
     
 
-    
+    #nested*for loop-pattern program
+
+'''for i in range(3):
+     for j in range(0,i+1):
+
+       print("*",end="")
+     print("")
+'''
+'''for i in range(3):
+    for j in range(0,i+1):
+       print(j,end="")
+    print('')
+'''
+
+
+
+
+for i in range(5):
+    for j in range(1,i+1):
+        print(i,end="")
+    print('')
+
