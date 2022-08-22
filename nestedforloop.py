7cf0395aab7641e1a22b425890a96be25a72157a
Suresh5961 <Sureshkumarece87@gmail.com>
2022-08-22 10:55:48 +0530

    while loop

--- nestedforloop.py
+++ nestedforloop.py
@@ -4,10 +4,10 @@
     for j in range(2):  #-->(0,1)
 
         print(j)
-    print(i)'''
-    
+    print(i)
+    '''
 
-    #nested*for loop-pattern program
+  #nested*for loop-pattern program
 
 '''for i in range(3):
      for j in range(0,i+1):
@@ -21,11 +21,8 @@
     print('')
 '''
 
-
-
-
-for i in range(5):
-    for j in range(1,i+1):
-        print(i,end="")
-    print('')
-
+n=int(input("enter the value of n"))
+for i in range(n):
+    for j in range(0,i+1):
+        print(n-j,end="")
+    print("")
