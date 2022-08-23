50219ba2fd93789f96d2165bc405bde336ff94e3
Suresh5961 <Sureshkumarece87@gmail.com>
2022-08-23 10:15:38 +0530

    changes

---
+++ e.py
@@ -0,0 +1,8 @@
+'''for i in range(10):
+    for j in range(0,i+5):
+        print("*",end="")
+    print('')'''
+
+while(3>1):
+    print
+   
---
+++ whileLoop.py
@@ -0,0 +1,32 @@
+'''i=0
+while i<5:
+    print(i)
+    i+=1  #i=i+1'''
+
+
+'''i=10
+while(i>1):
+    print("aarthi")
+    i=i-1
+'''
+'''j=0
+while(j<=10):
+    print(j)
+    j=j+1
+
+'''
+'''#infinite loop
+while True:
+    print("helllo")
+'''
+
+#nested while loop
+
+i=2
+while i<3:
+    print("hello")
+    j=0
+    while j<2:
+        print("hai")
+        j+=1  #j=j+1
+    i+=1      #i=i+1
