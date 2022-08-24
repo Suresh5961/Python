09fda8f43327a5cdfa214b3b0d246e6d5e6dd111
Suresh5961 <Sureshkumarece87@gmail.com>
2022-08-24 11:00:43 +0530

    add list

---
+++ list.py
@@ -0,0 +1,41 @@
+A=[1,2,3,4,5,"tamil","english"]
+'''A.reverse()
+print(A)'''
+
+'''A.remove("tamil")
+print(A)
+print(A.pop(2))  #----> give a position to pop(remove)
+print(A)
+'''
+B=["maths","computer",1,2,3]
+A.extend(B)    #---> Extend function use add two list
+print(A)
+
+A.clear()
+print(B)
+
+'''C=[1,2,3,'S',"S"]
+D=C.copy()
+print(D)
+
+E=C.count("S")
+print(E)
+
+
+
+A1=[1000,2000,3000,4000,5000]
+print(max(A1))
+print(min(A1))
+print(sum(A1))
+
+#TWO Method to delete the number 
+#1.remove
+#2.pop
+
+A1.remove(1000)
+print(A1)
+
+print(A1.pop(0))
+print(A1)
+
+'''
