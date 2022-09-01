e87c4df05155bbf193f451af92cd81d4b46d423f
Suresh5961 <Sureshkumarece87@gmail.com>
2022-09-01 11:06:53 +0530

    dict

---
+++ dic.py
@@ -0,0 +1,44 @@
+'''# two position 1.key 2.value
+a={"name":"SS","age":21,"name2":"HH"}
+print(type(a),a)
+print(a["age"])
+print(len(a))
+
+
+#method 1.get  2.keys 3.value 4.add
+b={"name":"pp","age":21,"frds":["sam","ip","nithya"]}
+print(b)
+print(b["age"])
+
+c=b.get("name")
+print(c)
+c=b.keys()
+print(c)
+
+b["bike"]="Ns"   #-->add key and value
+print(b)
+
+c=b.values()  #--->only print  value
+print(c)
+
+b["age"]=26   #---->change values
+print(b)
+
+b.update({"name":"SH"})   #----> also change values
+print(b)
+'''
+#remove method 1.pop ,2.popitem 3.del 4.clear
+x={"name":"oo","age":"21","frds":["sam","ip","nithya"],"food":"biriyani"}
+print(x)
+
+print(x.pop("name"))
+#print(x)
+
+x.popitem()   # remove  last item 
+print(x)
+
+del x["age"]
+print(x)
+
+x.clear()
+print(x)
