e77b25dfee74ba358740e1555b57d264e85e2b60
Suresh5961 <Sureshkumarece87@gmail.com>
2022-09-05 10:02:11 +0530

    function

---
+++ fun.py
@@ -0,0 +1,22 @@
+def A():
+    print("hai")
+A()
+
+def add():
+    a=10
+    b=20
+    c=a+b
+    print(c)
+
+add()             #function calling
+
+#parameter function or arguments
+
+def b(age):
+    print(age)
+b(21)
+
+def c(degree):
+    print(degree)
+   
+c("engg")
---
+++ fun1.py
@@ -0,0 +1,19 @@
+'''def city():
+    a=input("tell ur fav city")
+    if a == "chennai":
+        print("mall")
+    elif a=="salem":
+        print("temple")
+    elif a=="madurai":print("flower")
+    else:print("invalid")
+
+city()'''
+
+def grede(qual,mark):
+    if qual =="B.E" and mark ==70:
+        print("u r eligible in 1st class")
+    elif qual =="B.E" and mark ==55:
+        print("ur eligible in 2nd class")
+    else:print("ur not eligible")
+grede("B.E",70)
+grede("B.E",45)
