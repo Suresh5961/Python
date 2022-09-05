49867f7acdb25d472c2900e9f598081054ab73d2
Suresh5961 <Sureshkumarece87@gmail.com>
2022-09-05 10:07:27 +0530

    fun

--- fun1.py
+++ fun1.py
@@ -9,11 +9,20 @@
 
 city()'''
 
-def grede(qual,mark):
+'''def grede(qual,mark):
     if qual =="B.E" and mark ==70:
         print("u r eligible in 1st class")
     elif qual =="B.E" and mark ==55:
         print("ur eligible in 2nd class")
     else:print("ur not eligible")
 grede("B.E",70)
-grede("B.E",45)
+grede("B.E",45)'''
+
+
+def vote(name,age,location):
+    if age == 18: print(name,"is eligible for vooting",location)
+    elif age<=25: print(name,"eligible for vooting",location)
+    else:print("ur not eligible")
+vote("aarthi",18,"salem")
+vote("preethi",20,"chennai")
+vote(age=22,location="salem",name="SS")
