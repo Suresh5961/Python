ccd36809069d430511f3c7f2c90d5442b7865187
Suresh5961 <Sureshkumarece87@gmail.com>
2022-09-16 00:10:00 +0530

    add

--- Forloop.py
+++ Forloop.py
@@ -1,20 +1,21 @@
 #for loop
 
-for a in range(5):
+'''for a in range(5):
     print(a)
 
 for b in range(10):
     print("aarthi")
 
 for c in "suresh":
-    print(c)
+    print(c)'''
 
 for i in range(10,20,1):
     print(i)
 
-'''for j in range(30,50,2):
-     print(j)
-'''
-
 '''for k in range (20,10,-2):
-    print(k)'''
+    print(k)
+
+for j in range(30,50,2):
+     print(j)'''
+
+
--- e.py
+++ e.py
@@ -9,15 +9,53 @@ if (posword1<=8):
 else:
     print("need more character")'''
 
+#positive or negative number using class #def function
+'''class a:
+    def q(self,a):
+        if a>=0:
+            print("positive")
+        else:
+            print("-")
 
-class a:
+class b(a):
     def E(self,a):
         if a>0:
             print("postive num")
         else:
             print("negative num")
-s=a()
-s.E(-1)
-        
-        
+s=b()
+s.E(-11)
+s.q(22)
+'''
+
+'''a=[1,2,3,4]
+b=["one","two","three","four"]'''
+
+'''a=[1,2,3,4]                             #$$$
+b=["one","two","three","forur"]
+
+
+for k in range(4):
+    print(a[k],b[k])'''
+
+
+'''for a in range (20,10,-1):
+    print(a)'''
+
+'''for a in range (3,60,3):
+    print(a)'''
+
+'''
+a=int(input("enter n"))
+
+for b in range (3,a,3):
+    print(b)'''
+
+
+i=3                           #taples
+for k in range (1,11):    
+    t=k*i
+    print(t)
       
+
+
