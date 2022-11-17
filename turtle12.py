4f6537c608a829ef2dadeebe011a9234d1cf2198
Suresh5961 <Sureshkumarece87@gmail.com>
2022-11-17 15:51:47 +0530

    turtle

---
+++ turtle12.py
@@ -0,0 +1,24 @@
+import turtle as t
+import time
+t.shape('turtle')
+t.speed(5)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.penup()
+t.forward(100)
+t.right(90)
+t.pendown()
+t.forward(200)
+t.left(180)
+
+
+
+time.sleep(10)
